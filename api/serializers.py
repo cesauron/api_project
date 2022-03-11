98a5fc687442059506b6465a91fad8905f1f7872
Cesar <cesauronis@gmail.com>
2022-03-11 22:11:35 +0100

    inicio

---
+++ api/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from api.models import Post
+class PostSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Post  
+        exclude = ['is_removed', 'created', 'modified']
